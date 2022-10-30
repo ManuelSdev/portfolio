eaf702e1430d84ef7312779b9b77f3ceef28bc3d
Manuel S <masanchezzm@gmail.com>
2022-10-30 09:17:58 +0100

    2

---
+++ src/pages/test.js
@@ -0,0 +1,16 @@
+
+
+
+import React from 'react'
+
+const Test = () => {
+    return (
+        <h1
+            className="text-3xl font-bold underline"
+        >
+            Hello world!
+        </h1>
+    )
+}
+
+export default Test
